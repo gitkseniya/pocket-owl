e26454e0fec2c1c3e0b19051d8b563a282c5ec3f
Kseniya <kseniya.kudelich@gmail.com>
2025-02-18 22:27:52 -0700

    Add fc plugin to bot menu

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -41,8 +41,9 @@ class ChatGPTTelegramBot:
         self.commands = [
             BotCommand(command='help', description=localized_text('help_description', bot_language)),
             BotCommand(command='reset', description=localized_text('reset_description', bot_language)),
-            BotCommand(command='stats', description=localized_text('stats_description', bot_language)),
-            BotCommand(command='resend', description=localized_text('resend_description', bot_language))
+            BotCommand(command='family_const', description=localized_text('family_const_descrpription', bot_language)),
+            # BotCommand(command='stats', description=localized_text('stats_description', bot_language)),
+            # BotCommand(command='resend', description=localized_text('resend_description', bot_language))
         ]
         # If imaging is enabled, add the "image" command to the list
         if self.config.get('enable_image_generation', False):
